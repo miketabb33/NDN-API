29a4513a50cbefcc4f15624fb0d32c112fdd78ee
Michael Tabb <miketabb33@gmail.com>
2019-07-06 13:16:24 -0400

    Move json handling into method

--- app/controllers/nasa_daily_news_controller.rb
+++ app/controllers/nasa_daily_news_controller.rb
@@ -5,28 +5,28 @@ class NasaDailyNewsController < ApplicationController
   def index 
     api_url = "https://api.nasa.gov/planetary/apod"
     api_key = "O5XtjFT6wV1o5zLwNDhhwf8giPbWhlasYL24H69p"
-    full_link = api_url + "?api_key=" + api_key
+    api_link = api_url + "?api_key=" + api_key
 
-    uri = URI.parse(full_link)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-    request = Net::HTTP::Get.new(uri.request_uri)
-
-    response = http.request(request)
+    response = getJson(api_link)
     json = JSON.parse(response.body)
-
     newNDN = NasaDailyNews.new(copyright: json["copyright"], date: json["date"], explanation: json["explanation"], 
                                hdurl: json["hdurl"], media_type: json["media_type"], service_version: json["service_version"], 
                                title: json["title"], url: json["url"])
-    
     attempToSave(newNDN, json["date"])
-    
     @NDN = NasaDailyNews.all
   end
 
   private
+
+    #Json handling
+    def getJson(link)
+      uri = URI.parse(link)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      request = Net::HTTP::Get.new(uri.request_uri)
+      return http.request(request)
+    end
 
     #DB saving methods
     def attempToSave(nasaEntry, jsonDate)

57481300912e3f0c16d8eb5e8cf71b2e999b863c
Michael Tabb <miketabb33@gmail.com>
2019-07-06 12:14:08 -0400

    Request and display nasa information

--- app/controllers/nasa_daily_news_controller.rb
+++ app/controllers/nasa_daily_news_controller.rb
@@ -1,5 +1,20 @@
 class NasaDailyNewsController < ApplicationController
+  require "net/https"
+  require "uri"
+
   def index 
-    render html: "Hello"
+    api_url = "https://api.nasa.gov/planetary/apod"
+    api_key = "O5XtjFT6wV1o5zLwNDhhwf8giPbWhlasYL24H69p"
+    full_link = api_url + "?api_key=" + api_key
+
+    uri = URI.parse(full_link)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(uri.request_uri)
+
+    response = http.request(request)
+    @json = JSON.parse(response.body)
   end
 end

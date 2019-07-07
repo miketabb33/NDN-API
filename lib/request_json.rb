cf90918555b996e4a387877026020c5f911f63ac
Michael Tabb <miketabb33@gmail.com>
2019-07-07 14:52:11 -0400

    Nest save data in successful json request

--- lib/request_json.rb
+++ lib/request_json.rb
@@ -1,8 +1,11 @@
 class RequestJSON
   require "net/https"
   require "uri"
+  require "persistent_data"
 
-  def getNasaJSON()
+  def getNasaJSONandSave()
+    persistentData = PersistentData.new
+
     api_url = "https://api.nasa.gov/planetary/apod"
     api_key = "O5XtjFT6wV1o5zLwNDhhwf8giPbWhlasYL24H69p"
     api_link = api_url + "?api_key=" + api_key
@@ -14,12 +17,16 @@ class RequestJSON
     request = Net::HTTP::Get.new(uri.request_uri)
     response = http.request(request)
 
-    if response.code != "200" 
-      return JSON.parse(response.body)
+    if response.code == "200" 
+      json = JSON.parse(response.body)
+      newNDN = NasaDailyNews.new(copyright: json["copyright"], date: json["date"], explanation: json["explanation"], 
+                             hdurl: json["hdurl"], media_type: json["media_type"], service_version: json["service_version"], 
+                             title: json["title"], url: json["url"])
+      persistentData.attempToSave(newNDN, json["date"])
     else
       puts "DEVELOPER NOTES:"
       puts "Error in retrieving json data from nasa.com. Code: #{response.code}"
-      NotificationMailer.error_with_json_request(response)
+      NotificationMailer.error_with_json_request(response).deliver_now
     end
   end
 end
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -1,18 +1,12 @@
-require "persistent_data"
 require "request_json"
 
 desc "This task is called by the Heroku scheduler add-on"
 task :adding_nasa_entry => :environment do
   puts "Adding nasa entry for today..."
 
-  persistentData = PersistentData.new
   requestJSON = RequestJSON.new 
 
-  json = requestJSON.getNasaJSON()
-  newNDN = NasaDailyNews.new(copyright: json["copyright"], date: json["date"], explanation: json["explanation"], 
-                             hdurl: json["hdurl"], media_type: json["media_type"], service_version: json["service_version"], 
-                             title: json["title"], url: json["url"])
-  persistentData.attempToSave(newNDN, json["date"])
+  requestJSON.getNasaJSONandSave()
   
   puts "done."
 end

aafef1c691c634f0d1ff267079933cdbdee61138
Michael Tabb <miketabb33@gmail.com>
2019-07-07 11:42:53 -0400

    Add cron job to update nasa db

--- app/controllers/nasa_daily_news_controller.rb
+++ app/controllers/nasa_daily_news_controller.rb
@@ -3,15 +3,6 @@ class NasaDailyNewsController < ApplicationController
   require "request_json"
 
   def index 
-    persistentData = PersistentData.new
-    requestJSON = RequestJSON.new 
-
-    response = requestJSON.getNasaJSON()
-    json = JSON.parse(response.body)
-    newNDN = NasaDailyNews.new(copyright: json["copyright"], date: json["date"], explanation: json["explanation"], 
-                               hdurl: json["hdurl"], media_type: json["media_type"], service_version: json["service_version"], 
-                               title: json["title"], url: json["url"])
-    persistentData.attempToSave(newNDN, json["date"])
     @NDN = NasaDailyNews.all
   end
 

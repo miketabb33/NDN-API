0fd98ba51e92ce4604cd35c7a10c2842159b4311
Michael Tabb <miketabb33@gmail.com>
2019-09-10 16:27:08 -0400

    Refactor controller

--- app/controllers/nasa_daily_news_controller.rb
+++ app/controllers/nasa_daily_news_controller.rb
@@ -2,14 +2,24 @@ class NasaDailyNewsController < ApplicationController
 
   def index 
     if Rails.env.production?
-      if params[:id] == ENV['KEY']
-        @NDN = NasaDailyNews.all.order(created_at: :desc)
-      else
-        render html: "Permission is not granted to access this page"
-      end
+      key_check
     else
-      @NDN = NasaDailyNews.all.order(created_at: :desc)
+      @NDN = get_entries
     end
   end
 
+  private 
+
+  def key_check 
+    if params[:id] == ENV['KEY']
+      @NDN = get_entries
+    else
+      render html: "Permission is not granted to access this page"
+    end
+  end
+
+  def get_entries
+    NasaDailyNews.all.order(created_at: :desc)
+  end
+  
 end
